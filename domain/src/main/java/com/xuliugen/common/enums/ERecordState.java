a66d86504b42c5a439baa7dca25672d0922fc166
liugen.xu <1573876303@qq.com>
2016-01-11 16:09:11 +0800

    update

--- domain/src/main/java/com/xuliugen/common/enums/ERecordState.java
+++ domain/src/main/java/com/xuliugen/common/enums/ERecordState.java
@@ -12,6 +12,22 @@ public enum ERecordState {
     public Integer value;
     public String desc;
 
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
     ERecordState(Integer value, String desc) {
         this.value = value;
         this.desc = desc;
--- domain/src/main/java/com/xuliugen/common/enums/EResponseState.java
+++ domain/src/main/java/com/xuliugen/common/enums/EResponseState.java
@@ -13,6 +13,22 @@ public enum EResponseState {
     public Integer value;
     public String desc;
 
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
     EResponseState(Integer code, String msg) {
         this.value = code;
         this.desc = msg;
